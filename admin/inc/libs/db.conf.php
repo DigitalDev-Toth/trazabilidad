f0e7745c6ba648c4c0f829013b5b456a067392f8
juandelsolar@gmail.com <Juan Francisco>
2014-07-30 20:35:33 -0400

    seteo de bd en digitaldev.org

--- admin/inc/libs/db.conf.php
+++ admin/inc/libs/db.conf.php
@@ -1,12 +1,8 @@
 <?
-$username = "juan";
+$hostname = "digitaldev.org";
+$username = "postgres";
 $password = "justgoon";
 $dbname = "traza";
-<<<<<<< HEAD
-$hostname = "digitaldev.org";
-=======
-$hostname = "localhost";
->>>>>>> 4f093acfc79f5ac08d754e50f66e87374801ab3c
 $port = "5432";
 
 //relative directory
