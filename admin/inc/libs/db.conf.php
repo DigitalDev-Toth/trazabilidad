b7af55ad3d67f3c3012c6007ec4690829b4fd969
srh-work <ply.srh@gmail.com>
2014-08-26 17:36:54 -0400

    Pull enzo

---
+++ admin/inc/libs/db.conf.php
@@ -0,0 +1,10 @@
+<?
+$hostname = "digitaldev.org";
+$username = "postgres";
+$password = "justgoon";
+$dbname = "traza";
+$port = "5432";
+
+//relative directory
+$filesPathDirectory = "../../../uploads/";
+?>
