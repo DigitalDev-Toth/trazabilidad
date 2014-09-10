2ba1e943d7946a1ae826f40acfc599bd82fff33e
juandelsolar@gmail.com <Juan Francisco>
2014-09-10 16:49:59 -0300

    cambios en comet.js
    no se mergearon

--- visor/js/libs/comet.js
+++ visor/js/libs/comet.js
@@ -38,18 +38,10 @@ var Comet = function (data_url) {
         if (indexComet !== 0) {
             var data = $.parseJSON(response.msg);
         	console.log(response.msg);
-<<<<<<< HEAD
-            if(response.msg[0]=='{'){
-                if (data.comet === 'tothtem' || data.comet === 'module') {
-                    MAKE.goTo(data.comet, data.rut, data.action, data.newticket, data.datetime, data.module, data.submodule);
-
-                } else if (data.comet === 'submodule') {
-=======
             if (data.comet === 'tothtem' || data.comet === 'module') {
                 MAKE.goTo(data.comet, data.rut, data.action, data.newticket, data.datetime, data.module, data.submodule);
             } else if (data.comet === 'submodule') {
                 if(SUBMODULES[data.id]!=undefined) {
->>>>>>> juan
                     if(data.state==='activo') {
                         MODULES[SUBMODULES[data.id]].submodules[data.id].setActive();
                     } else if(data.state==='inactivo') {
@@ -58,22 +50,22 @@ var Comet = function (data_url) {
                         MODULES[SUBMODULES[data.id]].submodules[data.id].blink(0);
                     }
                 }
-            }else if(response.msg[0]=='['){
-                for(var i=0;i<data.length;i++){
-                    if (data[i].comet === 'tothtem' || data[i].comet === 'module') {
-                        MAKE.goTo(data[i].comet, data[i].rut, data[i].action, data[i].newticket, data[i].datetime, data[i].module, data[i].submodule);
+            }
+            /*for(var i=0;i<data.length;i++){
+                console.log('hi');
+                if (data[i].comet === 'tothtem' || data[i].comet === 'module') {
+                    MAKE.goTo(data[i].comet, data[i].rut, data[i].action, data[i].newticket, data[i].datetime, data[i].module, data[i].submodule);
 
-                    } else if (data[i].comet === 'submodule') {
-                        if(data[i].state==='activo') {
-                            MODULES[SUBMODULES[data[i].id]].submodules[data[i].id].setActive();
-                        } else if(data[i].state==='inactivo') {
-                            MODULES[SUBMODULES[data[i].id]].submodules[data[i].id].setInactive();
-                        } else if(data[i].state==='blink') {
-                            MODULES[SUBMODULES[data[i].id]].submodules[data[i].id].blink(0);
-                        }
+                } else if (data[i].comet === 'submodule') {
+                    if(data[i].state==='activo') {
+                        MODULES[SUBMODULES[data[i].id]].submodules[data[i].id].setActive();
+                    } else if(data[i].state==='inactivo') {
+                        MODULES[SUBMODULES[data[i].id]].submodules[data[i].id].setInactive();
+                    } else if(data[i].state==='blink') {
+                        MODULES[SUBMODULES[data[i].id]].submodules[data[i].id].blink(0);
                     }
                 }
-            }
+            }*/
 
         } else {
             indexComet = 1;
