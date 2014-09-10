8d4cd9e0b37fa8aa294fd177e97815e5dade94b1
juandelsolar@gmail.com <Juan Francisco>
2014-09-10 16:47:19 -0300

    cambios instantáneos de estado y pestañeo
    en submodulo

---
+++ admin/inc/modules/updateSubmoduleState.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors',1);
+include("../libs/db.class.php");
+if(isset($_GET['state'])) {
+	$db = new DB();
+	$id = $_GET['id'];
+	$state = $_GET['state'];
+	if($state=='activo') {
+		$state = 'inactivo';
+	} else {
+		$state = 'activo';
+	}
+	$sql = "UPDATE submodule SET state='$state' WHERE id=$id";
+	$db->doSql($sql);
+	echo $state;
+} else {
+	echo "missing data!";
+}
+?>
